b9bd151e3d009271bc2dca36100e20da2687a51d
Joe Inman <joe.inman@ross-robotics.co.uk>
2024-11-06 14:45:54 +0000

    Filesystem Agnostic File Reading

--- include/EmbedYAML/EmbedYAML.hpp
+++ include/EmbedYAML/EmbedYAML.hpp
@@ -13,37 +13,18 @@ using EYFileOpenFunction = std::function<int(EmbedYAML*, std::string)>;
 using EYFileCloseFunction = std::function<int(EmbedYAML*, std::string)>;
 using EYReadCharFunction = std::function<std::optional<char>(EmbedYAML*)>;
 
-enum class TokenType {
-    STRING,
-    NUMBER,
-    COLON,
-    HYPHEN,
-    INDENT,
-    EOL,
-    EOF_TOKEN,
-    ERROR
-};
-
-struct Token {
-    TokenType type;
-    std::string value;
-};
-
 class EmbedYAML {
 public:
     EmbedYAML(EYFileOpenFunction open, EYFileCloseFunction close, EYReadCharFunction read_char, void* user_context = nullptr);
     ~EmbedYAML();
 
-    std::vector<Token> parseFile(std::string filename);
+    bool parseFile(std::string filename);
 
     void* getUserContext() const { return m_user_context; }
 private:
     EYFileOpenFunction m_file_open_function;
     EYFileCloseFunction m_file_close_function;
     EYReadCharFunction m_read_char_function;
-
-    char next_char;
-    int current_indent;
 
     // Allow user context variable
     void* m_user_context;
--- src/EmbedYAML.cpp
+++ src/EmbedYAML.cpp
@@ -2,8 +2,14 @@
 
 namespace EmbedYAML {
 
-EmbedYAML::EmbedYAML(EYFileOpenFunction open, EYFileCloseFunction close, EYReadCharFunction read_char, void* user_context)
-    : m_file_open_function(open), m_file_close_function(close), m_read_char_function(read_char), m_user_context(user_context)
+EmbedYAML::EmbedYAML(EYFileOpenFunction open,
+                     EYFileCloseFunction close,
+                     EYReadCharFunction read_char,
+                     void* user_context)
+    : m_file_open_function(open),
+      m_file_close_function(close),
+      m_read_char_function(read_char),
+      m_user_context(user_context)
 {
 }
 
@@ -11,113 +17,25 @@ EmbedYAML::~EmbedYAML()
 {
 }
 
-std::vector<Token> EmbedYAML::parseFile(std::string filename)
+bool EmbedYAML::parseFile(std::string filename)
 {
     if (m_file_open_function(this, filename) < 0)
-    {
-        return std::vector<Token>{Token{TokenType::ERROR, "Failed to open file"}};
-    }
-
-    std::vector<Token> tokens;
-    int current_indent_level = 0;
-    bool is_new_line = true;
+        return false;
 
     while (true)
     {
         auto c = m_read_char_function(this);
-
-        if (!c.has_value())
-        {
-            tokens.emplace_back(Token{TokenType::EOF_TOKEN, ""});
+        if (c.has_value())
+            printf("%c", c.value());
+        else
             break;
-        }
-
-        if (c.value() == '\n')
-        {
-            tokens.emplace_back(Token{TokenType::EOL, ""});
-            is_new_line = true;
-            continue;
-        }
-
-        if (is_new_line && (c.value() == ' ' || c.value() == '\t'))
-        {
-            int indent_count = 0;
-
-            // Count indentation level at the beginning of a line
-            while (c.has_value() && (c.value() == ' ' || c.value() == '\t'))
-            {
-                indent_count++;
-                c = m_read_char_function(this);
-            }
-
-            // Register the indentation token based on the number of spaces
-            if (indent_count > current_indent_level)
-            {
-                tokens.emplace_back(Token{TokenType::INDENT, ""});
-                current_indent_level = indent_count;
-            }
-
-            // Continue processing after indentation
-            if (!c.has_value())
-            {
-                tokens.emplace_back(Token{TokenType::EOF_TOKEN, ""});
-                break;
-            }
-            is_new_line = false;
-        }
-
-        if (c.value() == ':')
-        {
-            tokens.emplace_back(Token{TokenType::COLON, ""});
-            continue;
-        }
-
-        if (c.value() == '-')
-        {
-            tokens.emplace_back(Token{TokenType::HYPHEN, ""});
-            continue;
-        }
-
-        // Handle strings and numbers
-        if (std::isalnum(c.value()) || c.value() == '_')
-        {
-            std::string value;
-            value.push_back(c.value());
-
-            while (true)
-            {
-                c = m_read_char_function(this);
-                if (!c.has_value() || c.value() == ' ' || c.value() == ':' || c.value() == '\n' || c.value() == '-')
-                {
-                    // Put back the last character if needed (not consumed yet)
-                    if (c.has_value() && c.value() != '\n')
-                    {
-                        // You can cache the character for the next iteration
-                    }
-
-                    // Determine if it's a number or string
-                    if (std::all_of(value.begin(), value.end(), ::isdigit))
-                    {
-                        tokens.emplace_back(Token{TokenType::NUMBER, value});
-                    }
-                    else
-                    {
-                        tokens.emplace_back(Token{TokenType::STRING, value});
-                    }
-                    break;
-                }
-                value.push_back(c.value());
-            }
-        }
     }
+    printf("\n");
 
     if (m_file_close_function(this, filename) < 0)
-    {
-        tokens.emplace_back(Token{TokenType::ERROR, "Failed to close file"});
-    }
+        return false;
 
-    return tokens;
+    return true;
 }
-
 
 } // namespace EmbedYAML
